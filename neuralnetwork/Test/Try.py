580d020b994287c858d2cf050df296cd16fc8da6
piedpiperG <3135484689@qq.com>
2023-12-23 15:46:27 +0800

    feat(Generative_Model):完成普通GAN模型

--- neuralnetwork/Test/Try.py
+++ neuralnetwork/Test/Try.py
@@ -1,14 +1,14 @@
-Dict = {
-    "class": {
-        "student": {
-            "name": "Mike",
-            "marks": {
-                "physics": 70,
-                "history": 80
-            }
-        }
-    }
-}
+import torch
+import torch.nn as nn
 
-# print(Dict['class']['student']['marks']['physics'])
-print(Dict["class"]["student"]["marks"]["physics"])
+# 定义转置卷积层
+transposed_conv = nn.ConvTranspose2d(in_channels=3, out_channels=64, kernel_size=4, stride=2, padding=1)
+
+# 随机生成输入数据
+input_data = torch.randn(1, 3, 32, 32)  # (batch_size, in_channels, height, width)
+
+# 进行转置卷积操作
+output_data = transposed_conv(input_data)
+
+# 输出结果的形状
+print(output_data.shape)
--- neuralnetwork/课设/data.py
+++ neuralnetwork/课设/data.py
@@ -1,8 +1,6 @@
 import string
 import torch
 from torch.utils.data import Dataset, DataLoader
-from torch.nn.utils.rnn import pad_sequence
-import torch.nn as nn
 from PIL import Image
 import json
 import os
@@ -10,47 +8,55 @@ import numpy as np
 
 
 class ImageTextDataset(Dataset):
-    def __init__(self, json_file_path, image_folder_path, embedding_dim):
+    def __init__(self, json_file_path, image_folder_path, max_seq_length):
         """
         Args:
             json_file_path (string): JSON文件的路径，包含图片名称和对应的描述。
             image_folder_path (string): 包含图片的文件夹路径。
-            embedding_dim (int): 词向量维度。
+            max_seq_length (int): 最大序列长度。
         """
         with open(json_file_path, 'r', encoding='utf-8') as file:
             self.descriptions = json.load(file)
         self.image_folder_path = image_folder_path
+        self.max_seq_length = max_seq_length
         self.word_to_index = self.create_vocab(self.descriptions)
-        vocab_size = len(self.word_to_index)
-        self.embedding = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embedding_dim)
+
+    def preprocess_text(self, text):
+        """
+        对文本进行预处理，将句号与单词分离。
+        """
+        # 使用空格替换句号，确保句号被视为独立的单词
+        text = text.replace('.', ' . ')
+        return text.lower().translate(str.maketrans('', '', string.punctuation.replace('.', '')))
 
     def create_vocab(self, descriptions):
         word_freq = {}
         for _, description in descriptions.items():
-            words = description.lower().translate(str.maketrans('', '', string.punctuation)).split()
+            preprocessed_text = self.preprocess_text(description)
+            words = preprocessed_text.split()
             for word in words:
                 if word not in word_freq:
                     word_freq[word] = 1
                 else:
                     word_freq[word] += 1
 
-        # 添加特殊符号
-        vocab = {'<sos>': 0, '<eos>': 1, '<unk>': 2}
-        idx = 3  # 从索引3开始添加其他单词
-
+        vocab = {'<pad>': 0, '<sos>': 1, '<eos>': 2, '<unk>': 3, '.': 4}  # 将句号添加为特殊符号
+        idx = 5  # 从索引5开始添加其他单词
         for word in sorted(word_freq, key=word_freq.get, reverse=True):
-            vocab[word] = idx
-            idx += 1
+            if word not in vocab:  # 防止重复添加
+                vocab[word] = idx
+                idx += 1
 
         return vocab
 
     def text_to_indices(self, text):
-        text = text.lower().translate(str.maketrans('', '', string.punctuation))
-        # 添加开始符和结束符
+        preprocessed_text = self.preprocess_text(text)
+        words = preprocessed_text.split()
         indices = [self.word_to_index['<sos>']] + \
-                  [self.word_to_index.get(word, self.word_to_index['<unk>']) for word in text.split()] + \
+                  [self.word_to_index.get(word, self.word_to_index['<unk>']) for word in words] + \
                   [self.word_to_index['<eos>']]
-        return indices
+        indices += [self.word_to_index['<pad>']] * (self.max_seq_length - len(indices))
+        return indices[:self.max_seq_length]
 
     def __len__(self):
         return len(self.descriptions)
@@ -68,26 +74,42 @@ class ImageTextDataset(Dataset):
 
         indices = self.text_to_indices(description)
         indices_tensor = torch.tensor(indices, dtype=torch.long)
-        description_vectors = self.embedding(indices_tensor)
 
-        return {'image': image, 'description_vectors': description_vectors, 'description': description}
+        return {'image': image, 'indices': indices_tensor, 'description': description}
 
 
-def collate_fn(batch):
-    images = [item['image'] for item in batch]
-    descriptions = [item['description_vectors'] for item in batch]
-    description_word = [item['description'] for item in batch]
-    descriptions_padded = pad_sequence(descriptions, batch_first=True, padding_value=0)
-    images = torch.stack(images, dim=0)
-    return {'image': images, 'description_vectors': descriptions_padded, 'description': description_word}
+def get_max_seq_length(json_file_path):
+    """
+    计算给定 JSON 文件中最长文本描述的长度。
+
+    Args:
+        json_file_path (string): JSON文件的路径。
+
+    Returns:
+        int: 最大序列长度。
+    """
+    with open(json_file_path, 'r', encoding='utf-8') as file:
+        descriptions = json.load(file)
+
+    max_length = 0
+    for _, description in descriptions.items():
+        # 移除标点符号，并以空格分割成单词
+        words = description.lower().translate(str.maketrans('', '', string.punctuation)).split()
+        # 计算长度（加2是因为<sos>和<eos>标记）
+        length = len(words) + 2
+        if length > max_length:
+            max_length = length
+
+    return max_length
 
 
 # 使用示例
-embedding_dim = 300  # 假设的词向量维度
+max_seq_length = max(get_max_seq_length('word2vec/train_captions.json'),
+                     get_max_seq_length('word2vec/test_captions.json'))
 json_file_path = 'word2vec/test_captions.json'  # JSON文件路径
 image_folder_path = 'E:\杂项下载\课设数据集\images'  # 图片文件夹路径
-dataset = ImageTextDataset(json_file_path, image_folder_path, embedding_dim)
-dataloader = DataLoader(dataset, batch_size=4, collate_fn=collate_fn)
+dataset = ImageTextDataset(json_file_path, image_folder_path, max_seq_length)
+dataloader = DataLoader(dataset, batch_size=4)
 
 for batch in dataloader:
-    print(batch['image'], batch['description_vectors'])
+    print(batch['image'], batch['indices'])
